fdd62c2954e064743b90ab86fe65828991819be5
Natalia Zalivako <zalivako.natalia@gmail.com>
2020-09-22 20:06:39 +0300

    complete day2 tasks

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,15 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
 
+        if (i >= 9) System.out.println("Многоэтажный дом");
+        else if (i >= 5) System.out.println("Среднеэтажный дом");
+        else if (i >= 1) System.out.println("Малоэтажный дом");
+        else System.out.println("Ошибка ввода");
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,17 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y;
 
+        if (x >= 5) y = (x * x - 10) / (x + 7);
+        else if (x > -3) y = (x + 3) * (x * x - 2);
+        else y = 420;
+
+        System.out.println(y);
     }
 }
