da08d7e0b62faf2c721cd7bd39942d72ec73da32
Natalia Zalivako <zalivako.natalia@gmail.com>
2020-09-23 23:15:40 +0300

    complete day3 tasks

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,40 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String city;
+        while (true) {
+            city = scanner.nextLine();
+            if (city.equals("Stop")) {
+                break;
+            }
+            switch (city) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин ":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double dividend;
+        double divider;
+        int counter = 0;
+        while (counter < 5) {
+            dividend = scanner.nextDouble();
+            divider = scanner.nextDouble();
+            if (divider == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(dividend / divider);
+            counter++;
+        }
     }
 }
