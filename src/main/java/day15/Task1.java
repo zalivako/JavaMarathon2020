0600bb58759bf720727da0a3789ef125d80bfcc2
Natalia Zalivako <zalivako.natalia@gmail.com>
2020-10-06 16:00:09 +0300

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,26 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File fileIn = new File("src/main/resources/shoes.csv");
+        File fileOut = new File("src/main/resources/missing_shoes.txt");
 
+        try (Scanner scanner = new Scanner(fileIn); PrintWriter printWriter = new PrintWriter(fileOut)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] shoes = line.split(";");
+
+                if (Integer.parseInt(shoes[shoes.length - 1]) == 0) {
+                    printWriter.println(line.replace(";", ", "));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
