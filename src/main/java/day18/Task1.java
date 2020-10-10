184900b8cfcc6a39c200d4003918491ef0bcc4b8
Natalia Zalivako <zalivako.natalia@gmail.com>
2020-10-10 15:21:36 +0300

    complete day18 tasks

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,19 +1,29 @@
 package day18;
 
-/**
- * @author Neil Alishev
- */
 public class Task1 {
     public static void main(String[] args) {
-        int[] numbers = {-402, -50, -249, -15, -665, -311};
-
-        System.out.println(recursionSum(numbers, 0));
+        int[] numbers = {1, 10, 1241};
+        System.out.println(Sum(numbers)); // version 1 - первое решение
+        System.out.println(recursionSum(numbers, 0)); // version 2 - решение после просмотра автотестов
     }
 
-    public static int recursionSum(int[] numbers, int i) {
-        if (i == numbers.length)
-            return 0;
+    //    version 1
+    public static int Sum(int[] numbers) {
+        return recursionSumV1(numbers, numbers.length - 1);
+    }
 
-        return numbers[i] + recursionSum(numbers, i + 1);
+    public static int recursionSumV1(int[] numbers, int i) {
+        if (i == 0) {
+            return numbers[i];
+        }
+        return numbers[i] + recursionSumV1(numbers, (i - 1));
+    }
+
+    //    version 2
+    public static int recursionSum(int[] numbers, int i) {
+        if (i == numbers.length) {
+            return 0;
+        }
+        return numbers[i] + recursionSum(numbers, (i + 1));
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -1,20 +1,17 @@
 package day18;
 
-/**
- * @author Neil Alishev
- */
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println(count7(717771237)); // 5
+        System.out.println(count7(1772377770)); // 6
     }
 
-    public static int count7(int number) {
-        if (number == 0)
+    public static int count7(int i) {
+        if (i == 0) {
             return 0;
-
-        if (number % 10 == 7)
-            return 1 + count7(number / 10);
-        else
-            return count7(number / 10);
+        }
+        if (i % 10 == 7) {
+            return 1 + count7(i / 10);
+        }
+        return count7(i / 10);
     }
 }
